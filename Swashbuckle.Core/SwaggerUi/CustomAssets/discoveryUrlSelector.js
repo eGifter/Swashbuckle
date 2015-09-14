6eeba5328ad4b6ffa66687655eac1ba763759489
tristan <tristan@egifter.com>
2015-09-14 16:16:17 -0400

    aligned discoveryUrlSelector methods

--- Swashbuckle.Core/SwaggerUi/CustomAssets/discoveryUrlSelector.js
+++ Swashbuckle.Core/SwaggerUi/CustomAssets/discoveryUrlSelector.js
@@ -1,25 +1,35 @@
 ﻿(function () {
     var DiscoveryUrlSelector = Backbone.View.extend({
-        render: function() {
+        render: function () {
+            var that = this;
             // Don't re-render on subsequent reloads
-            var defaultVal = this.$el.val()
-            if (this.$el.prop('tagName') != 'SELECT') {
+            var defaultVal = that.$el.val();
+            if (that.$el.prop('tagName') != 'SELECT') {
                 var select = $('<select id="input_baseUrl" name="baseUrl"></select>');
                 select
                     .css('margin', '0')
                     .css('border', '1px solid gray')
                     .css('padding', '3px')
-                    .css('width', '400px')
+                    .css('width', '80%')
                     .css('font-size', '0.9em');
 
-                var rootUrl = this.options.rootUrl;
-                _.each(this.options.discoveryPaths, function(path) {
-                    var option = $('<option>' + rootUrl + "/" + path + '</option>');
+                var rootUrl = that.options.rootUrl;
+                _.each(that.options.discoveryPaths, function (path) {
+                    var option = $('<option value="' + path + '">' + path.replace('/swagger', '') + '</option>');
                     select.append(option);
                 });
 
-                select.val(defaultVal);
-                this.$el.replaceWith(select);
+                var versionIndex = window.location.search.match(/versionIndex=([^&]*)/);
+                if (versionIndex != null)
+                    select.val(that.options.discoveryPaths[versionIndex[1]]);
+                else
+                    select.val(defaultVal);
+                that.$el.replaceWith(select);
+
+                $('#input_baseUrl').on('change', function () {
+                    //console.log('selected index: ' + that.options.discoveryPaths.indexOf($(this).val()));
+                    setGetParameter('versionIndex', that.options.discoveryPaths.indexOf($(this).val()));
+                });
             }
             return this;
         }
