c7f9999fe5133f6c5bca49701791a7664f7f47a1
tristan <t@mrtristan.com>
2018-09-05 13:09:20 -0400

    logo path fix

--- Swashbuckle.Core/SwaggerUi/CustomAssets/discoveryUrlSelector.js
+++ Swashbuckle.Core/SwaggerUi/CustomAssets/discoveryUrlSelector.js
@@ -32,7 +32,7 @@
                     setGetParameter('versionIndex', that.options.discoveryPaths.indexOf($(this).val()));
                 });
 
-                $('<div id="egLogo" />').append('<img id="egLogo" src="//egifter.com/Images/themes/eGifter/egifter-logo120.png" />').prependTo('.sticky-nav');
+                $('<div id="egLogo" />').append('<img id="egLogo" src="//egifter.com/Images/themes/eGifter/egifter-logo-wide-blue.svg" />').prependTo('.sticky-nav');
                 $('h4[data-toggle]:not(.collapsed)').trigger('click');
                 setTimeout(function() {
                     $('h4[data-toggle]:not(.collapsed)').trigger('click');
